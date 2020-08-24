84c1f4fae49af2a4a57bc52998293524700b017e
Sudhanshu <perfectsudh@gmail.com>
2020-08-24 19:18:17 +0530

    feat(ui): adds placeholder register page

---
+++ ui/src/pages/register.tsx
@@ -0,0 +1,48 @@
+import { Box, Button } from "@chakra-ui/core"
+import { Form, Formik } from "formik"
+import React from "react"
+import { InputField } from "../components/input-field"
+import { Wrapper } from "../components/wrapper"
+
+interface RegisterProps {}
+
+const Register: React.FC<RegisterProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              variantColor="teal"
+              isLoading={isSubmitting}
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  )
+}
+
+export default Register
